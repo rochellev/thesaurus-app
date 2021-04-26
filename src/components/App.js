8f09f404ab01252f550082260843640f0b7ccd9a
rochellev <rochellevroberts@utexas.edu>
2021-04-26 16:20:24 -0700

    add sample data to work with later

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import HomeView from './home/HomeView'
+import "./App.css";
+import HomeView from "./home/HomeView";
 
 const App = () => {
   return (
     <div className="App">
-     <text>This is App</text>
-     <HomeView />
+      <text>This is App</text>
+      <HomeView />
     </div>
   );
-}
+};
 
 export default App;
