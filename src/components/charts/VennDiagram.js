803df613b3d95107e17862f064d8b9f2d943d353
rochellev <rochellevroberts@utexas.edu>
2021-04-30 16:18:23 -0700

    make it so circles overlap, but have two charts

--- src/components/charts/VennDiagram.js
+++ src/components/charts/VennDiagram.js
@@ -5,27 +5,20 @@ import * as am4plugins_venn from "@amcharts/amcharts4/plugins/venn";
 import "../App.css";
 
 const VennDiagram = props => {
-  const vennDiagram = useRef(null);
-  useEffect(() => {
-    // create venn diagram chart instance
-    let chart = am4core.create("chartdiv", am4plugins_venn.VennDiagram);
+  // Create chart
+  var chart = am4core.create("chartdiv", am4plugins_venn.VennDiagram);
 
-    // create and configure series
-    let series = chart.series.push(new am4plugins_venn.VennSeries());
-    series.dataFields.category = "name";
-    series.dataFields.value = "value";
-    series.dataFields.intersections = "sets";
-    series.data = [
-      { name: "A", value: 10 },
-      { name: "B", value: 10 },
-      { name: "C", value: 3, set: ["A", "B"] }
-    ];
+  // Create and configure series
+  var series = chart.series.push(new am4plugins_venn.VennSeries());
+  series.dataFields.category = "name";
+  series.dataFields.value = "value";
+  series.dataFields.intersections = "sets";
+  series.data = [
+    { name: "A", value: 10 },
+    { name: "B", value: 10 },
+    { name: "C", value: 3, sets: ["A", "B"] }
+  ];
 
-    vennDiagram.current = chart;
-    return () => {
-      chart.dispose();
-    };
-  },[]);
   return <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>;
 };
 
