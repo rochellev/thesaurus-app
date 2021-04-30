179e00b9970dbc97d77e57fbe4c6788d8698e34f
rochellev <rochellevroberts@utexas.edu>
2021-04-30 16:35:08 -0700

    add example for forced directed tree

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import HomeView from "./home/HomeView";
 import VennDiagram from "./charts/VennDiagram";
+import ForcedDirectedTree from './charts/ForceDirectedTree';
 const App = () => {
   return (
     <div className="App">
-      <VennDiagram />
-      {/* <HomeView /> */}
+   <ForcedDirectedTree />
     </div>
   );
 };
