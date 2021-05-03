67fe12e705fc5cb7ada5e94ccf63927cb28f30fd
rochellev <rochellevroberts@utexas.edu>
2021-05-03 16:14:23 -0700

    add imports to make use of layout effect and useRef

--- src/components/charts/ForceDirectedTree.js
+++ src/components/charts/ForceDirectedTree.js
@@ -1,10 +1,13 @@
+import React, { useRef, useLayoutEffect } from "react";
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import * as am4plugins_forceDirected from "@amcharts/amcharts4/plugins/forceDirected";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import "../App.css";
 
 const ForceDirectedTree = () => {
 
-  
+
   // Create chart
   var chart = am4core.create(
     "chartdiv",
