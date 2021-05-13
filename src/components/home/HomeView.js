d975978d3bf7365d8971566635a0af7db41aba94
rochellev <rochellevroberts@utexas.edu>
2021-05-13 15:49:10 -0700

    app works with store, can get the word data. now that i know store works, i can start implementing api calls in actions

--- src/components/home/HomeView.js
+++ src/components/home/HomeView.js
@@ -1,146 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import SynonymTree from "./SynonymTree";
-import { selectWord } from "../../actions";
-// sample input: cool
-const data = {
-  headword: "cool",
-  syn_list: [
-    {
-      definition:
-        "having or showing a lack of friendliness or interest in others",
-      syns: [
-        "aloof",
-        "antisocial",
-        "asocial",
-        "buttoned-up",
-        "cold",
-        "cold-eyed, detached",
-        "distant",
-        "dry",
-        "frosty",
-        "offish",
-        "remote",
-        "standoff",
-        "standoffish",
-        "unbending",
-        "unclubbable",
-        "unsociable"
-      ]
-    },
-    {
-      definition: "free from emotional or mental agitation",
-      syns: [
-        "calm",
-        "collected",
-        "composed",
-        "coolheaded",
-        "equal",
-        "level",
-        "limpid",
-        "peaceful",
-        "placid",
-        "possessed",
-        "recollected",
-        "sedate",
-        "self-composed",
-        "self-possessed",
-        "serene",
-        "smooth",
-        "together",
-        "tranquil",
-        "undisturbed",
-        "unperturbed",
-        "unruffled",
-        "unshaken",
-        "untroubled",
-        "unworried"
-      ]
-    },
-    {
-      definition: "having a low or subnormal temperature",
-      syns: [
-        "algid",
-        "arctic",
-        "bitter",
-        "bone-chilling, chill",
-        "chilly, cold",
-        "coldish",
-        "coolish",
-        "freezing",
-        "frigid",
-        "frosty",
-        "gelid",
-        "glacial",
-        "ice-cold",
-        "icy",
-        "nipping",
-        "nippy",
-        "numbing",
-        "polar",
-        "shivery",
-        "snappy",
-        "wintry"
-      ]
-    },
-    {
-      definition: "lacking in friendliness or warmth of feeling",
-      syns: [
-        "antiseptic",
-        "arctic",
-        "brittle",
-        "chill",
-        "chilly",
-        "clammy",
-        "cold",
-        "cold-blooded",
-        "cold-eyed",
-        "coldish",
-        "frigid",
-        "frosty",
-        "frozen",
-        "gelid",
-        "glacial",
-        "hard-eyed",
-        "icy",
-        "uncordial",
-        "unfriendly",
-        "unsympathetic",
-        "wintry"
-      ]
-    },
-    {
-      definition: "(slang) being in the latest or current fashion",
-      syns: [
-        "à la mode",
-        "au courant",
-        "chic",
-        "exclusive",
-        "fashionable",
-        "fresh [slang]",
-        "happening",
-        "hip",
-        "in",
-        "modish",
-        "sharp",
-        "smart",
-        "snappy",
-        "stylish",
-        "supercool",
-        "swell",
-        "swish",
-        "trendy",
-        "voguish"
-      ]
-    }
-  ]
-};
 
-const HomeView = () => {
-  const [wordData, setWordData] = useState(data);
+const HomeView = ({ wordData }) => {
   const [chartData, setChartData] = useState([
     {
-      name: data.headword,
+      name: wordData.headword,
       children: []
     }
   ]);
@@ -159,7 +24,7 @@ const HomeView = () => {
     }
 
     setChartData(currData);
-  }, [chartData, wordData.syn_list]);
+  }, []);
 
   return (
     <div>
@@ -168,6 +33,7 @@ const HomeView = () => {
     </div>
   );
 };
+
 const mapStateToProps = state => {
   return {
     wordData: state.wordData
