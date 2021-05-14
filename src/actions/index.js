e8bee65c7c5fce66e042152c582ec0dff93aa85f
rochellev <rochellevroberts@utexas.edu>
2021-05-14 14:46:52 -0700

    add action creators for begin, success and fail actions that will use in synonym reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
 import merriamWebster from "../apis/merriamWebster";
-import {WORD_SELECTED, FETCH_SYNONYMS_BEGIN, FETCH_SYNONYMS_SUCCESS, FETCH_SYNONYMS_FAIL} from "./types";
+import {
+  WORD_SELECTED,
+  FETCH_SYNONYMS_BEGIN,
+  FETCH_SYNONYMS_SUCCESS,
+  FETCH_SYNONYMS_FAIL
+} from "./types";
 
 // currently only setting up reducer for getting data
 export const selectWord = word => {
@@ -9,7 +14,18 @@ export const selectWord = word => {
   };
 };
 
-export const fetchSynonyms = (headword) => {
-  const response = await merriamWebster.get()
-} 
- 
+// export const fetchSynonyms = (headword) => {
+//   const response = await merriamWebster.get()
+// }
+
+export const fetchSynonymsBegin = () => {
+  return { type: FETCH_SYNONYMS_BEGIN };
+};
+
+export const fetchSynonymsSuccess = synonyms => {
+  return { type: FETCH_SYNONYMS_SUCCESS, payload: { synonyms } };
+};
+
+export const fetchSynonymsFail = error => {
+  return { type: FETCH_SYNONYMS_FAIL, payload: { error } };
+};
