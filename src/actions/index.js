fc3f8564aa98f0d4fcea199022ab02d8d1c8be98
rochellev <rochellevroberts@utexas.edu>
2021-05-14 14:39:58 -0700

    add types and rename api file merriam webster

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,15 @@
-// action creator
+import merriamWebster from "../apis/merriamWebster";
+import {WORD_SELECTED, FETCH_SYNONYMS_BEGIN, FETCH_SYNONYMS_SUCCESS, FETCH_SYNONYMS_FAIL} from "./types";
 
 // currently only setting up reducer for getting data
 export const selectWord = word => {
   return {
-    type: "WORD_SELECTED",
+    type: WORD_SELECTED,
     payload: word
   };
 };
+
+export const fetchSynonyms = (headword) => {
+  const response = await merriamWebster.get()
+} 
+ 
