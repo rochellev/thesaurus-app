edfed3086f530239533945d4d8b53acb82f5d1d9
rochellev <rochellevroberts@utexas.edu>
2021-05-21 16:34:08 -0700

    finish refactoring of fetchSynonymsBegin to handle errors

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ import {
   FETCH_SYNONYMS_FAIL
 } from "./types";
 
-// currently only setting up reducer for getting data
+// hard-coded data -- for developing
 export const selectWord = word => {
   return {
     type: WORD_SELECTED,
@@ -16,9 +16,7 @@ export const selectWord = word => {
 
 // fetch API data, handle success/error
 export const fetchSynonymsBegin = headword => async dispatch => {
-  // signal starting api call
   dispatch({ type: FETCH_SYNONYMS_BEGIN });
-  // changed to return this conditional sort of. rather than set a variable to response.
   return await merriamWebster
     .get(`/cool?key=${process.env.REACT_APP_MERRIAM_WEBSTER_KEY}`)
     .then(response =>
@@ -26,13 +24,3 @@ export const fetchSynonymsBegin = headword => async dispatch => {
     )
     .catch(error => dispatch({ type: FETCH_SYNONYMS_FAIL, payload: error }));
 };
-
-// export const fetchSynonymsSuccess = synonyms => {};
-
-// export const fetchSynonymsFail = error => {
-//   return { type: FETCH_SYNONYMS_FAIL, payload: { error } };
-// };
-
-// export const transformData = () => {
-
-// }
--- src/reducers/synonymReducer.js
+++ src/reducers/synonymReducer.js
@@ -4,11 +4,8 @@ import {
   FETCH_SYNONYMS_FAIL
 } from "../actions/types";
 
-// begin as in loading
-
 const INITIAL_STATE = {
   loading: null,
-  fetchSuccess: null,
   error: null,
   data: {},
   treeData: []
@@ -21,6 +18,7 @@ export default (state = INITIAL_STATE, action) => {
     case FETCH_SYNONYMS_SUCCESS:
       return { ...state, loading: false, error: null, data: action.payload };
     case FETCH_SYNONYMS_FAIL:
+      // y/n -- override existing data when error
       return { ...state, loading: false, error: action.payload };
     default:
       return state;
