6ec198a55d9b3b194c5dcf5904d943dbaa7b8485
rochellev <rochellevroberts@utexas.edu>
2021-05-26 15:14:36 -0700

    add file for SearchForm

---
+++ src/components/home/SearchForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Form, FormSpy, Field } from 'react-final-form'
+
+const SearchForm = ({subscription}) => {
+  return(
+    <Form />
+  )
+}
