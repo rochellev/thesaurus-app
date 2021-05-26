350d2d7764dfa6639152c6fd2030edc85b2caeb9
rochellev <rochellevroberts@utexas.edu>
2021-05-26 11:34:33 -0700

    some final comment and import removals, need to refactor homeView, but will do on a different branch

--- src/components/home/HomeView.js
+++ src/components/home/HomeView.js
@@ -2,38 +2,17 @@ import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import SynonymTree from "./SynonymTree";
 import { fetchSynonymsBegin } from "../../actions";
-import merriamWebster from "../../apis/merriamWebster";
 
-const HomeView = ({ fetchSynonymsBegin, wordData, synonyms }) => {
-  const [chartData, setChartData] = useState([
-    {
-      name: wordData.headword,
-      children: []
-    }
-  ]);
+
+const HomeView = ({ fetchSynonymsBegin, synonyms }) => {
+
   // run initial
   useEffect(() => {
     fetchSynonymsBegin("happy");
   }, []);
-  useEffect(() => {
-    let currData = chartData;
-    // for each sense
-    // make new obj with def and synonyms
-    // push that to root node children
-    for (let senseObj of wordData.syn_list) {
-      let sense = { name: senseObj.definition, value: 50, children: [] };
-      for (let [i, synonym] of senseObj.syns.entries()) {
-        sense.children.push({ name: synonym, value: 1 });
-      }
-      currData[0].children.push(sense);
-    }
 
-    setChartData(currData);
-  }, []);
 
-  // const showSyns = synonyms => {
-  //   return;
-  // };
+
   const handleSearch = () => {
     fetchSynonymsBegin("happy");
   };
