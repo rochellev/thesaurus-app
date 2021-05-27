aa6c4fc8f0bd7c9a9b5ff6d17db8445f4466e1c4
rochellev <rochellevroberts@utexas.edu>
2021-05-27 13:45:18 -0700

    start working on SynonymSearch component

--- src/components/home/SynonymSearch.js
+++ src/components/home/SynonymSearch.js
@@ -1,6 +1,30 @@
 import React from "react";
 import { Form, Field } from "react-final-form";
 
-const SynonymSearch = () => {};
+const SynonymSearch = () => {
+  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+  const onSubmit = async values => {
+    await sleep(300);
+    window.alert(JSON.stringify(values, 0, 2));
+  };
+
+  const validateSearchTerm = values => {
+    const errors = {};
+    if (!values.searchTerm) {
+      errors.searchTerm = "Missing Search Term";
+    }
+    // add check for only letters
+    return errors;
+  };
+  return (
+    <Form
+      onSubmit={onSubmit}
+      initialValues={{ searchTerm: "happy" }}
+      validate={validateSearchTerm(values)}
+      
+    />
+  );
+};
 
 export default SynonymSearch;
