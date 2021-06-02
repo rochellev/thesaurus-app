f1d5eee705463a98f0f4f1abe44e49c15c3e2c88
rochellev <rochellevroberts@utexas.edu>
2021-06-02 11:50:08 -0700

    add top padding to search box

--- src/components/home/SearchBox.js
+++ src/components/home/SearchBox.js
@@ -3,7 +3,7 @@ import { connect, useSelector, useDispatch } from "react-redux";
 
 export const SearchBox = () => {
   return (
-    <div className="ui large icon input">
+    <div className="ui large icon input" style={{paddingTop: 25}}>
       <input type="text" placeholder="Search large..." />
       <i className="search link icon"></i>
     </div>
