902389bc0a1715280041c7e1d047f4af2b196089
rochellev <rochellevroberts@utexas.edu>
2021-06-03 14:47:05 -0700

    add onChange property for SearchBox

--- src/components/home/SearchBox.js
+++ src/components/home/SearchBox.js
@@ -2,11 +2,23 @@ import React, { useState, useEffect } from "react";
 import { connect, useSelector, useDispatch } from "react-redux";
 
 export const SearchBox = () => {
-  const [searchTerm, setSearchTerm] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const debounceInput = term => {
+    // debugger;
+    // setTimeout(term => setSearchTerm(term), 100);
+    setSearchTerm(term);
+  };
   return (
-    <div className="ui large icon input" style={{ marginTop: 15 }}>
-      <input type="text" placeholder="Search large..." />
-      <i className="search link icon"></i>
+    <div>
+      <div>The searchTerm is {searchTerm}</div>
+      <div className="ui large icon input" style={{ marginTop: 15 }}>
+        <input
+          type="text"
+          placeholder="Search large..."
+          onChange={e => debounceInput(e.target.value)}
+        />
+        <i className="search link icon"></i>
+      </div>
     </div>
   );
 };
