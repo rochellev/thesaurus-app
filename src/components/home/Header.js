4323f686daa65677dd802a12864ef7ecbdcad7a8
rochellev <rochellevroberts@utexas.edu>
2021-06-04 11:25:41 -0700

    add file for header component

---
+++ src/components/home/Header.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+export const Header = () => {
+  return(
+    <div>
+      <div>
+        <h1>Thesaurus for Us</h1>
+        
+      </div>
+    </div>
+  )
+}
